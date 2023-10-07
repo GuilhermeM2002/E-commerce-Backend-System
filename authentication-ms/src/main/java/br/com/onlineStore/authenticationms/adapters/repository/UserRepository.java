95f4cd5ea75a1b6a837d95cd47d338dbeec5322a
GuilhermeM2002 <guilhermemartinsribeiro2002@gmail.com>
2023-10-06 21:09:42 -0300

    add: repositories "user" and "role"

---
+++ authentication-ms/src/main/java/br/com/onlineStore/authenticationms/adapters/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.onlineStore.authenticationms.adapters.repository;
+
+import br.com.onlineStore.authenticationms.core.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
