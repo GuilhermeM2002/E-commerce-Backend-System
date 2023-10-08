53a0ed49ae0d08ae05d8be7dcb32fa61bdd036e4
GuilhermeM2002 <guilhermemartinsribeiro2002@gmail.com>
2023-10-07 21:26:46 -0300

    add: security configuration and token jwt

--- authentication-ms/src/main/java/br/com/onlineStore/authenticationms/adapters/repository/UserRepository.java
+++ authentication-ms/src/main/java/br/com/onlineStore/authenticationms/adapters/repository/UserRepository.java
@@ -2,8 +2,10 @@ package br.com.onlineStore.authenticationms.adapters.repository;
 
 import br.com.onlineStore.authenticationms.core.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findUserByEmail(String email);
 }
