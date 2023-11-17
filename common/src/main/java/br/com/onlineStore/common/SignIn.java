b07232070cf494cf5af371ae1b6057d782222be4
GuilhermeM2002 <guilhermemartinsribeiro2002@gmail.com>
2023-11-17 20:51:17 -0300

    add: module common with SingIn class

---
+++ common/src/main/java/br/com/onlineStore/common/SignIn.java
@@ -0,0 +1,15 @@
+package br.com.onlineStore.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignIn {
+    private String email;
+    private String token;
+}
