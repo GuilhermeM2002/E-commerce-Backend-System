ba9bb90bcd944a70b798367d2bc3752431b87735
GuilhermeM2002 <guilhermemartinsribeiro2002@gmail.com>
2024-02-26 22:24:13 -0300

    add: register address

--- order-ms/src/main/java/br/com/onlineStore/orderms/application/useCasesImpl/RegisterAddressUseCaseImpl.java
+++ order-ms/src/main/java/br/com/onlineStore/orderms/application/useCasesImpl/RegisterAddressUseCaseImpl.java
@@ -28,22 +28,22 @@ public class RegisterAddressUseCaseImpl implements RegisterAddressUseCase {
         return mapper.map(address, AddressDto.class);
     }
 
-@Override
-public void validateAddressDto(AddressDto addressDto) {
-    var fields = new HashMap<String, String>();
-    fields.put("CEP", addressDto.getCep());
-    fields.put("City", addressDto.getCity());
-    fields.put("Neighborhood", addressDto.getNeighborhood());
-    fields.put("Number", addressDto.getNumber());
-    fields.put("State", addressDto.getState());
-    fields.put("Complement", addressDto.getComplement());
-    fields.put("Street", addressDto.getStreet());
+    @Override
+    public void validateAddressDto(AddressDto addressDto) {
+        var fields = new HashMap<String, String>();
+        fields.put("CEP", addressDto.getCep());
+        fields.put("City", addressDto.getCity());
+        fields.put("Neighborhood", addressDto.getNeighborhood());
+        fields.put("Number", addressDto.getNumber());
+        fields.put("State", addressDto.getState());
+        fields.put("Complement", addressDto.getComplement());
+        fields.put("Street", addressDto.getStreet());
 
-    for (Map.Entry<String, String> entry : fields.entrySet()) {
-        if (StringUtils.isEmpty(entry.getValue())) {
-            throw new IllegalArgumentException(entry.getKey() + " cannot be null or empty");
+        for (Map.Entry<String, String> entry : fields.entrySet()) {
+            if (StringUtils.isEmpty(entry.getValue())) {
+                throw new IllegalArgumentException(entry.getKey() + " cannot be null or empty");
+            }
         }
     }
-}
 
 }
