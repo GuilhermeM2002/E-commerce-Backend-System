5bf37ea51f4d5a97c332c24a69d19e42e18de8fe
GuilhermeM2002 <guilherme.infor2@gmail.com>
2024-03-14 20:25:33 -0300

    fix: add correct kafka port

--- shoppingCart-ms/src/main/java/br/com/onlineStore/shoppingCartms/infra/config/ProducerKafkaConfig.java
+++ shoppingCart-ms/src/main/java/br/com/onlineStore/shoppingCartms/infra/config/ProducerKafkaConfig.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 
 @Configuration
 public class ProducerKafkaConfig {
-    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:29092}")
     private String bootstrapAddress;
 
     @Bean
